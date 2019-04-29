218233a595da8fc7cd91e9dad2cd790a217ba491
Ahmednagii <ahmed.nagi@extremesolution.com>
2019-04-29 04:14:43 +0200

    Add show case in a simple example

--- example/App.js
+++ example/App.js
@@ -1,29 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Recommendation } from 'react-native-recommendation'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+textList=["FROM 9 -> 10","FROM 6 -> 8","FROM 5 -> 4","FROM 2 -> 3","FROM 0 -> 1"]
 
-type Props = {};
-export default class App extends Component<Props> {
+emojiList=[
+  require('./assets/ic_love.png'),
+  require('./assets/ic_happy.png'),
+  require('./assets/ic_confused.png'),
+  require('./assets/ic_sad.png'),
+  require('./assets/ic_angry.png')
+]
+
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Recommendation 
+        titleText={"Recomendation Test"}
+        selectedColor={'red'}
+        selectedTextColor={'white'}
+        unSelectedTextColor={'black'}
+        selectedSize={30}
+        // reactionTextList={textList}
+        // reactionList={emojiList}
+        max={10}
+        reactionsEnabled/>
       </View>
     );
   }
@@ -35,15 +37,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
